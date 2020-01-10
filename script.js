0a61c50ae704bc2eb7e295e81ea824826c27d473
gosha <info@elektrik-chelyabinsk.ru>
2020-01-10 15:45:21 +0500

    perviyraz

---
+++ script.js
@@ -0,0 +1,64 @@
+'use strict';
+
+let a = 5;
+console.log(a);
+
+var number = 5;
+var string = "Hello";
+var sym = Symbol();
+var boolean = true;
+null;
+undefined;
+var obj = {};
+
+console.log(4 / 0);
+console.log('string' * 9);
+
+let something;
+console.log(something);
+
+let persone = {
+    name: "Jhon",
+    age: 25,
+    isMarried: false
+};
+
+// console.log(persone.name);
+// console.log(persone.age);
+
+// console.log(persone["isMarried"]);
+
+
+let arr = ['plum.png', 'orange.jpg', 'apple.bmp'];
+
+console.log(arr[0]);
+
+// alert("hello world!")
+
+// let answer = confirm("Are you here?")
+// console.log(answer);
+
+// let answer = prompt("Есть ли Вам 18?", "Да");
+
+// console.log(typeof(answer));
+
+// console.log("arr" + " - object");
+// console.log(4 + " - object");
+
+let incr = 10,
+    decr = 10;
+
+// incr++;
+// decr--;
+
+console.log(incr++);
+console.log(decr--);
+
+
+console.log(5%2);
+console.log("2" === 2);
+
+let isCheked = false,
+isClose = true;
+
+console.log(!isCheked || isClose);
